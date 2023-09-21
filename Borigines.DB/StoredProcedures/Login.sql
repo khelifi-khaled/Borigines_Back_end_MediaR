1e71df010993dd984996d4693a3407ad33219c25
Khaled KHELIFI <khaled.khelifi@iramps.email>
2023-09-21 10:52:31 +0200

    edit user is active default val, add Login SP

---
+++ Borigines.DB/StoredProcedures/Login.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[Login]
+	@login NVARCHAR(250),
+	@pwd NVARCHAR (100)
+AS
+BEGIN 
+
+	DECLARE @salt NVARCHAR(100)
+	SET @salt = (SELECT Salt FROM Users WHERE [Login] = @login)
+
+	DECLARE @key NVARCHAR(64)
+	SET @key = (dbo.GetSecretKey)
+
+	DECLARE @pwd_hash VARBINARY(64)
+	SET @pwd_hash = HASHBYTES('SHA2_512', CONCAT(@salt,@key,@pwd,@salt))
+
+	IF EXISTS (SELECT * FROM Users WHERE [Login] = @login AND [Password] = @pwd_hash AND IsActive = 1)
+			SELECT 1 AS [Result]
+		ELSE
+			SELECT 0 AS [Result]
+
+END 
