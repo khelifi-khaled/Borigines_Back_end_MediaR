cec0a9b31a51633965f21c969cfbb69d5f952b57
Khaled KHELIFI <khaled.khelifi@iramps.email>
2023-09-21 11:03:51 +0200

    add UpdateUser

---
+++ Borigines.DB/StoredProcedures/UpdateUser.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[UpdateUser]
+	@fistName NVARCHAR(50),
+	@lastName NVARCHAR(50),
+	@login NVARCHAR(250),
+	@pwd NVARCHAR(100),
+	@id INT
+AS
+BEGIN 
+	DECLARE @salt NVARCHAR(100)
+	SET @salt = (dbo.GetSalt())
+
+	DECLARE @key NVARCHAR(64)
+	SET @key = (dbo.GetSecretKey)
+
+	DECLARE @pwd_hash VARBINARY(64)
+	SET @pwd_hash = HASHBYTES('SHA2_512', CONCAT(@salt,@key,@pwd,@salt))
+
+	UPDATE Users SET FirstName = @fistName, 
+					 LastName = @lastName, 
+					 [Login] = @login, 
+					 [Password] = @pwd_hash, 
+					 Salt = @salt 
+				 WHERE Id = @id
+END 
