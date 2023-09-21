d8c0b2003719ffe86687f32d5458865eb6506fb2
Khaled KHELIFI <khaled.khelifi@iramps.email>
2023-09-21 10:58:36 +0200

    add Register SP

---
+++ Borigines.DB/StoredProcedures/RegisterUser.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[RegisterUser]
+	@firstName NVARCHAR(50),
+	@lastName NVARCHAR(50),
+	@login NVARCHAR(250),
+	@pwd NVARCHAR (100)
+
+AS
+
+BEGIN 
+	DECLARE @salt NVARCHAR(100)
+	SET @salt = (dbo.GetSalt())
+
+	DECLARE @key NVARCHAR(64)
+	SET @key = (dbo.GetSecretKey)
+
+	DECLARE @pwd_hash VARBINARY(64)
+	SET @pwd_hash = HASHBYTES('SHA2_512', CONCAT(@salt,@key,@pwd,@salt))
+
+	INSERT INTO Users (FirstName, LastName, [Login], [Password], Salt)
+	VALUES (@firstName, @lastName, @login, @pwd_hash, @salt)
+
+END 
